99d9a1d686abab43558ee7648d7eec25d40f5bf3
Giri <yadagoud89@gmail.com>
2017-09-26 21:56:45 +0800

    yadatest

---
+++ src/DateDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class DateDemo {
+   public static void main(String args[]) {
+       // Instantiate a Date object
+       Date d = new Date();
+
+       // display time and date using toString()
+       System.out.println(d.toString());
+   }
+}
